c6e631e45000ba9ec40e3a51d43f6cac5a5851cc
s095339 <s095339@gmail.com>
2023-10-09 09:39:03 -0400

    read_back_coeff OK

--- fir/rtl/fir.v
+++ fir/rtl/fir.v
@@ -88,20 +88,39 @@ reg [(pADDR_WIDTH-1):0] araddr_buf;
 reg awready_reg,wready_reg;
 reg [(pADDR_WIDTH-1):0] awaddr_buf;
 reg [(pDATA_WIDTH-1):0] wdata_buf;
-//axilite to config module
+//axilite to config_ctrl module
 reg [1:0] axilite_req; //(req)
 reg [(pADDR_WIDTH-1):0] config_addr;
-reg [(pDATA_WIDTH-1):0] w_data, r_data;
+reg [(pDATA_WIDTH-1):0] w_data;
+// =====Config ctrl=====//
+//config_ctrl to axilite
+wire [(pDATA_WIDTH-1):0] r_data;
+//internal signal
+reg desti; //destination 0:config_reg 1.tap_ram
+reg desti_delay; //for r_data
+reg W_EN; // indicate whether change the value of tap_ram or config reg
+//confit ctrl to tap_ram_ctrl
+wire[3:0]  config_tap_WE;
+reg[(pDATA_WIDTH-1):0] config_tap_Di;
+reg[(pADDR_WIDTH-1):0] config_tap_A;
+reg[(pDATA_WIDTH-1):0] config_tap_Do;
+
+//config ctrl to cfg_reg_ctrl
+reg [8-1:0]config_ctrl_reg_out;
+wire config_ctrl_reg_wen;
+reg [8-1:0]config_ctrl_reg_in;
+// =====taps_ram_ctrl======== //
+reg [3:0]               tap_WE_reg;
+reg [(pDATA_WIDTH-1):0] tap_Di_reg;
+reg [(pADDR_WIDTH-1):0] tap_A_reg;
+
+
 
 // =====Axis-read ctrl======= //
 // =====Axis-write ctrl====== //
 // =====memory ctrl========== //
 // =====calculation unit===== //
 
-/*
-あなたの声(ap_start)は、道しるべ。
-何もない空に、私わ眠るよ
-*/
 
 
 
@@ -118,6 +137,11 @@ state
 3.finish: send axilite signal (ap_done, ap_idle) to testbench.
 */
 
+always@(posedge axis_clk or axis_rst_n)
+    if(~axis_rst_n)
+        state = STAT_IDLE;
+    else
+        state = STAT_IDLE;
 
 //******************************//
 // AxiLite Controller           //
@@ -161,8 +185,8 @@ always@* begin
     case(lite_state)
         LITE_wfinish:
             config_addr = awaddr_buf; // for write
-        //
-        //  config_addr = araddr_buf;
+        LITE_rreq:
+            config_addr = araddr_buf;
 
         default:
             config_addr = 12'd1; // addr not being used
@@ -187,6 +211,10 @@ always@*  begin
             w_data = {pDATA_WIDTH{1'b0}};
     endcase
 end
+
+
+
+
 //===lite_write=====
 
 assign awready = (lite_state == LITE_idle)? awready_reg : 1'b0;
@@ -274,7 +302,7 @@ always@(posedge axis_clk or negedge axis_rst_n)
             default:
                 awready_reg <= 1'b0;
         endcase
-always@(posedge axis_clk or negedge axis_rst_n)
+always@*
     if(~axis_rst_n)
         rvalid_reg <= 1'b0;
     else
@@ -305,6 +333,98 @@ always@(posedge axis_clk or negedge axis_rst_n)
 
 assign rdata = (lite_state == LITE_read)? 
                         r_data: {pDATA_WIDTH{1'b0}};
+//******************************//
+//Config ctrl                   //
+//******************************//
+//===internal signal=======
+//desti
+always@*begin
+    if(config_addr >= 12'h20)// taps
+        desti = 1;
+    else if(config_addr == 12'h10)// data_length(taps)
+        desti = 1;
+    else// config reg
+        desti = 0;
+end
+always@(posedge axis_clk or negedge axis_rst_n)
+    if(~axis_rst_n)
+        desti_delay <= 1'b0;
+    else
+        desti_delay <= desti;
+//W_EN (write or not)
+always@*
+    case(axilite_req)
+        2'b01://write
+            W_EN = 1'b1;
+        default:// preserved
+            W_EN = 1'b0;
+    endcase
+//===destination: tap_ram====
+//config_tap_A
+always@*begin
+    if(config_addr >= 12'h20)// taps
+        config_tap_A = config_addr - 12'h20; //transform to tap_addr
+    else if(config_addr == 12'h10)// data_length
+        config_tap_A = 12'h10<<2;
+    else
+        config_tap_A = 12'h10<<2;
+end
+//config_tap_WE
+assign config_tap_WE = (desti)? {4{W_EN}} : 4'd0;
+//config_tap_Di
+always@*
+    config_tap_Di = w_data;
+always@*
+    config_tap_Do = tap_Do;
+  
+//===destination: config_reg====
+//config_ctrl_reg_in
+always@*
+    config_ctrl_reg_in = w_data;
+//config_ctrl_reg_wen
+assign config_ctrl_reg_wen = (desti == 0)? W_EN:1'b0;
+//config_ctrl_reg_out
+    //寫在別的地方(cfg_reg_ctrl)
+
+//===config_ctrl to axilite
+//rdata
+assign r_data = (desti_delay)?config_tap_Do:32'd0; //這邊要寫config_ctrl_reg_out 但先這樣寫
+
+
+//******************************//
+// taps_ram_ctrl                //
+//******************************//
+assign tap_WE = tap_WE_reg;
+assign tap_Di = tap_Di_reg;
+assign tap_A = tap_A_reg;
+assign tap_EN = 1'b1;
+always@* begin
+    case(state)
+        STAT_IDLE:begin
+            tap_WE_reg = config_tap_WE;
+            tap_A_reg = config_tap_A;
+            tap_Di_reg = config_tap_Di;
+        end
+        /*
+        STAT_STORE_1_INPUT:begin
+            tap_WE_reg = 
+            tap_A_reg = 
+            tap_Di_reg = 
+        end
+        STAT_CAL:begin
+            
+        end
+        STAT_FINISH:begin
+
+        end
+        */
+        default:begin
+            tap_WE_reg = 0;
+            tap_A_reg = 0;
+            tap_Di_reg = 0;
+        end
+    endcase
+end
 
 //******************************//
 // FIR Dataflow                 //
